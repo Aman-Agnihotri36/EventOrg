1e0df5d8e81272fe7f00d89514bd89c6c744f42b
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-02-05 09:24:07 +0530

    end

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,13 @@ import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button"
 import { getAllEvents } from "@/lib/actions/event.actions";
-import { SearchParamProps } from "@/types";
+
 
 import Image from "next/image";
 import Link from "next/link";
 
-
-export default async function Home({ searchParams }: SearchParamProps) {
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+export default async function Home({ searchParams }: any) {
 
 
   const page = Number(searchParams?.page) || 1;

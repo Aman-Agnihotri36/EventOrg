ccf0f6430b6f14aa6694d021f322b188f924d89d
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-02-05 09:12:20 +0530

    completed

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -10,7 +10,7 @@ import React from 'react'
 
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-async function EventDetails({ params: { id }, searchParams }: SearchParamProps) {
+async function EventDetails({ params: { id }, searchParams }: any) {
 
 
 
